50bb9ec1e75eb5db4848e918a381ecbe9ae2c185
Dion Mar <dion.mar@gmail.com>
2025-05-20 10:10:48 -0300

    Com o codigo da Nasa

--- pipeline/exemplo_02.py
+++ pipeline/exemplo_02.py
@@ -1,7 +1,20 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=NASA_API_KEY"
+# Carrega as variáveis do .env
+load_dotenv()
+
+# Lê a variável de ambiente
+api_key = os.getenv("NASA_API_KEY")
+
+if not api_key:
+    print("❌ API_KEY não carregada.")
+else:
+    print("✅ API_KEY carregada com sucesso.")
+
+# Monta a URL com a chave lida do .env
+url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={api_key}"
 
 response = requests.get(url)
-
 print(response.json())
